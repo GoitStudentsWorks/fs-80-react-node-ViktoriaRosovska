a8c4d5161a01b0fa3b0f10a81befe12c8d394656
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-11-10 19:45:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       components: '/src/components',
     },
   },
-  base: '/fs-80-react-node-ViktoriaRosovska/',
+  base: '/fs-80-react-node-viktoriarosovska/',
 });
